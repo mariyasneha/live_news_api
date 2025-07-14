e1c5eddd9149f006014d4fe41bbfed0a64170b3b
mariyasneha <mariyasneha104@gmail.com>
2025-07-14 16:15:34 +0530

    second commit

---
+++ src/components/NewsNavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NewsNavBar = () => {
+  return (
+    <div>
+        <nav class="navbar navbar-expand-lg bg-info">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">NEWS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <Link class="nav-link active" aria-current="page" to="/">ViewAllNews</Link>
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default NewsNavBar
---
+++ src/components/ViewNews.jsx
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import NewsNavBar from './NewsNavBar'
+
+const ViewNews = () => {
+    const [newsData, ChangenewsData] = useState(
+        { "status": "ok", "totalResults": 34, "articles": [] }
+    )
+
+    const fetchData = () => {
+        axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=7c1139f61f624a3abc81786995a761c7").then(
+            (response) => {
+                ChangenewsData(response.data)
+            }
+        ).catch()
+    }
+    useEffect(() => { fetchData() }, [])
+
+    return (
+        <div>
+            <div className="container">
+                <NewsNavBar />
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            {newsData.articles.map(
+                                (value, index) => {
+                                    return (
+                                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                            <div class="card">
+                                                <img src={value.urlToImage} class="card-img-top" />
+                                                <div class="card-body">
+                                                    <h5 class="card-title">{value.title}</h5>
+                                                    <p class="card-text">{value.description}</p>
+                                                    <a href={value.url} target="_blank" class="btn btn-primary">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewNews
